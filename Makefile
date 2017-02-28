8deaa7ebf4c5dda71767f1e2f1495342535bed2b
aerth <aerth@riseup.net>
2017-02-28 02:10:46 -0800

    built-in options, noproxy

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 DEFAULT_SOCKS=127.0.0.1:1080
+#DEFAULT_SOCKS=none 	# short for none
+#DEFAULT_SOCKS=tor 		# short for 127.0.0.1:9050
 DEFAULT_RADIO=anonradio.net:8000
 DEFAULT_MOUNT=anonradio
 NAME=rstream
@@ -8,6 +10,6 @@ LDFLAGS=-X main.chunk=${CHUNK} -X main.defaultsocks=${DEFAULT_SOCKS}
 LDFLAGS2=-X main.defaultradio=${DEFAULT_RADIO} -X main.defaultmount=${DEFAULT_MOUNT} -X main.custom=${CUSTOM}
 all: build
 
-build: ${NAME}
+build:
 	go build -o "${NAME}" -ldflags "${LDFLAGS}" -ldflags "${LDFLAGS2}"
 	
--- rstream.go
+++ rstream.go
@@ -3,13 +3,15 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"net/url"
 	"os"
+	"strconv"
+	"strings"
 	"time"
-  "strconv"
+
 	"golang.org/x/net/proxy"
 )
+
 var chunk = "1440"
 var host string
 var defaultsocks = "socks5://127.0.0.1:1080"
@@ -18,20 +20,23 @@ var defaultmount = "anonradio"
 var chunksize = 1440
 var custom = "FALSE"
 
-func init(){
-  var err error
-  chunksize, err = strconv.Atoi(chunk)
-  if err != nil {
-    fmt.Println("Invalid chunk size")
-    os.Exit(1)
-  }
+func init() {
+	var err error
+	chunksize, err = strconv.Atoi(chunk)
+	if err != nil {
+		fmt.Println("Invalid chunk size")
+		os.Exit(1)
+	}
 }
 
 func openStream(socksurl, host, mountpoint string) {
 	mountpoint = strings.TrimPrefix(mountpoint, "/")
 	p := func() proxy.Dialer {
 		if socksurl == "none" {
-			return proxy.Direct
+			return proxy.FromEnvironment()
+		}
+		if socksurl == "tor" {
+			socksurl = "socks5://127.0.0.1:9050"
 		}
 		u, err := url.Parse(socksurl)
 		if err != nil {
@@ -54,59 +59,58 @@ func openStream(socksurl, host, mountpoint string) {
 	}
 	defer conn.Close()
 	fmt.Fprintln(os.Stderr, "connected")
-	fmt.Fprint(os.Stderr, "GET /"+mountpoint+" HTTP/1.0\r\n\r\n")
 	fmt.Fprint(conn, "GET /"+mountpoint+" HTTP/1.0\r\n\r\n")
 
 	for {
-	buff := make([]byte, chunksize) // 128kb/s ?
-	n, err := conn.Read(buff)
-	if err != nil {
-	fmt.Fprint(os.Stderr, n, err, " ")
-	} else {
-		fmt.Fprint(os.Stderr, n, "bytes ")
-	}
-	os.Stdout.Write(buff)
+		buff := make([]byte, chunksize) // 128kb/s ?
+		n, err := conn.Read(buff)
+		if err != nil {
+			fmt.Fprint(os.Stderr, n, err, " ")
+		} else {
+			if os.Getenv("DEBUG") != "" {fmt.Fprint(os.Stderr, n, "bytes ")}
+		}
+		os.Stdout.Write(buff)
 	}
 }
 
 func printUsage() {
 	fmt.Fprintln(os.Stderr, "remote mp3 to stdout")
-	fmt.Fprintln(os.Stderr,"proxy defaults to socks5://127.0.0.1:1080")
-	fmt.Fprintln(os.Stderr,"Usage: ", os.Args[0], " <host.server.com:port> <mountpoint> [<SOCKS>]")
-	fmt.Fprintln(os.Stderr,"Example (no proxy)", os.Args[0],"example.com:8000 techno none")
+	fmt.Fprintln(os.Stderr, "proxy defaults to socks5://127.0.0.1:1080")
+	fmt.Fprintln(os.Stderr, "Usage: ", os.Args[0], " <host.server.com:port> <mountpoint> [<SOCKS>]")
+	fmt.Fprintln(os.Stderr, "Example (no proxy)", os.Args[0], "example.com:8000 techno none")
 }
 
 func init() {
-if custom != "TRUE" { // CUSTOM="TRUE" in Makefile
-  if len(os.Args) != 3 && len(os.Args) != 4 {
-		printUsage()
-		os.Exit(1)
+	if custom != "TRUE" { // CUSTOM="TRUE" in Makefile
+		if len(os.Args) != 3 && len(os.Args) != 4 {
+			printUsage()
+			os.Exit(1)
+		}
 	}
-}
 
 }
 func main() {
-  var host, mountpoint, socks string
-  if len(os.Args) == 2 {
-    host = os.Args[1]
-  }
+	var host, mountpoint, socks string
+	if len(os.Args) == 2 {
+		host = os.Args[1]
+	}
 
-  if len(os.Args) == 3 {
-	host, mountpoint = os.Args[1], os.Args[2]
-  }
+	if len(os.Args) == 3 {
+		host, mountpoint = os.Args[1], os.Args[2]
+	}
 
-  if len(os.Args) == 4 {
-    socks = os.Args[3]
-  }
-  if host == "" {
-    host = defaultradio
-  }
-  if mountpoint == "" {
-    mountpoint = defaultmount
-  }
-  if socks == "" {
-    socks = defaultsocks
-  }
+	if len(os.Args) == 4 {
+		socks = os.Args[3]
+	}
+	if host == "" {
+		host = defaultradio
+	}
+	if mountpoint == "" {
+		mountpoint = defaultmount
+	}
+	if socks == "" {
+		socks = defaultsocks
+	}
 
 	openStream(socks, host, mountpoint)
 }
